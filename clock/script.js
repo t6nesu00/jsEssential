c579b02711e19c1a0f193e6a068f7a2c27ab869c
t6nesu00 <hlw.zigri@gamil.com>
2021-01-26 18:40:14 +0200

    analogue clock working

--- clock/script.js
+++ clock/script.js
@@ -4,19 +4,22 @@ const SECONDHAND = document.querySelector('.second');
 
 function runClock() {
     var date = new Date();
-console.log(date);
-var dateHour = date.getHours(); 
-var dateMinute = date.getMinutes();
-var dateSecond = date.getSeconds();
-console.log("Today's date is: "+"Hour: " + dateHour + " " + "Minute: "+dateMinute + " " + "Second: "+ dateSecond);
+    console.log(date);
+    var dateHour = date.getHours(); 
+    var dateMinute = date.getMinutes();
+    var dateSecond = date.getSeconds();
+    console.log("Today's date is: "+"Hour: " + dateHour + " " + "Minute: "+dateMinute + " " + "Second: "+ dateSecond);
 
-let hourPosition = (dateHour * 360/12) + ((dateMinute * 360/60)/12);
-let minutePosition = (dateMinute * 360/60) + ((dateMinute*360/60)/60);
-let secondPosition = dateSecond * 360/60;
+    let hourPosition = (dateHour * 360/12) + ((dateMinute * 360/60)/12);
+    let minutePosition = (dateMinute * 360/60) + ((dateMinute*360/60)/60);
+    let secondPosition = dateSecond * 360/60;
 
-HOURHAND.style.transform = "rotate(" + hourPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minutePosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secondPosition + "deg)";
+    HOURHAND.style.transform = "rotate(" + hourPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minutePosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secondPosition + "deg)";
+    
+    document.getElementById("date").innerHTML = date;
 }
 
 var interval = setInterval(runClock, 1000);
+
